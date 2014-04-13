25adc30c09a3e05e492cac54b3667d9f88f5a519
anthonyb28 <anthony@lssclan.org>
2014-04-12 22:23:30 -0400

    Basic Functionality
    Enable Editor and it automatically spawns a grid of hexes. Left click to
    enable them, right click to disable them, serialize button to save them.
    Then when it runs with deserialize checked, it will load the XML and
    spawn the hexes.

---
+++ HexEditor.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexEditor : MonoBehaviour {
+
+	private bool markDelete;
+	private Color originalColor;
+
+	void Awake () 
+	{
+		if(!Editor.instance.m_EnabledEditor) 
+			this.enabled = false;
+		else
+			originalColor = this.renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver()
+	{
+		if(this.enabled)
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				DeleteChild();
+				if(!markDelete)
+					Editor.instance.LeftClickedHex(this.gameObject);
+			}
+			if(Input.GetMouseButtonDown(1))
+			{
+				DeleteChild();
+				if(!markDelete)
+				{
+					markDelete = true;
+					Color deleteColor = this.renderer.material.color;
+					deleteColor = Color.red;
+					deleteColor.a = 0.1f;
+					this.renderer.material.color = deleteColor;
+					Editor.instance.RightClickHex(this.gameObject);
+				}
+				else
+				{
+					markDelete = false;
+					this.renderer.material.color = originalColor;
+				}
+			}
+		}
+
+	}
+
+	void DeleteChild()
+	{
+		if(this.transform.childCount > 0)
+		{
+			Transform old = this.transform.GetChild(0);
+			Destroy(old.gameObject);
+		}
+	}
+}
